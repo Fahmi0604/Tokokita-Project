d546722b23a49dbc790be6650c3f20d6f82bcf2e
nuruslaily <nuruslaily88@gmail.com>
2021-10-07 00:52:40 +0700

    update

--- input_laku.php
+++ input_laku.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>-- Pendataan Barang --</title>
+	<title>-- Toko Kita Pendataan Barang --</title>
 		<link rel="stylesheet" type="text/css" href="style_input.css">
 		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
